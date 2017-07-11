243f97c5e07d371833b3203f908038fb71aff3fe
ChaselWu <minowu@foxmail.com>
2017-07-11 22:44:41 +0800

    Simpilify scripts file

--- scripts/dev.js
+++ scripts/dev.js
@@ -1,12 +1,2 @@
 const WebpackDevRunner = require('../helpers/WebpackDevRunner')
-const path = require('path')
-
-function resolve (relativePath) {
-  return path.join(process.cwd(), relativePath)
-}
-
-module.exports = new WebpackDevRunner({
-  appPath: resolve('examples'),
-  sassResources: resolve('examples/human/config.scss'),
-  templateIndex: resolve('examples/index.html')
-}).run()
+module.exports = new WebpackDevRunner().run()
